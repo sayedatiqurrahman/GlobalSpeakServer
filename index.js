666827c0c2c0a63904746cf04cd44d8c9fb1e3a3
Atiqur Rahman <s.atiqurrahman2003@gmail.com>
2023-06-13 17:33:49 +0600

    finished students and Instructors task

--- index.js
+++ index.js
@@ -51,13 +51,16 @@ async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
         client.connect();
-        // Send a ping to confirm a successful connection
+        // Collections
         const LanguageCollection = client.db('SummerCamp').collection('Languages');
         const TestimonialsCollection = client.db('SummerCamp').collection('Testimonials');
         const UsersCollection = client.db('SummerCamp').collection('Users');
         const selectedClassesCollection = client.db('SummerCamp').collection('SelectedClasses');
         const enrolledClassesCollection = client.db('SummerCamp').collection('enrolledClasses');
 
+
+
+        // $============================================================$
         // routes
         app.post('/jwt', (req, res) => {
             const email = req.body;
@@ -80,7 +83,7 @@ async function run() {
 
 
         // Users Data post/create
-        app.post('/user', verifyJwt, async (req, res) => {
+        app.post('/user', async (req, res) => {
             const user = req.body;
             const email = user.email
             const query = { email: email }
@@ -162,18 +165,151 @@ async function run() {
             res.send(result)
         })
 
-        // payment history  routes
 
-        app.get('/paymentHistory', verifyJwt, async (req, res) => {
+
+        app.patch('/availableSeat', verifyJwt, async (req, res) => {
             const email = req.decoded.email;
             if (email) {
-                const result = await enrolledClassesCollection.find({ email }).toArray()
-                res.send(result)
+                const query = { email: email };
+
+                const enrolledClasses = await enrolledClassesCollection.find(query).toArray();
+                console.log(enrolledClasses);
+                const bookedIds = enrolledClasses.map(classItem => classItem.BookedId);
+
+
+
+                for (const bookedIdArray of bookedIds) {
+                    for (const bookedId of bookedIdArray) {
+                        // console.log('bookedId:', bookedId);
+
+                        await LanguageCollection.updateOne(
+                            { _id: new ObjectId(bookedId) },
+                            { $inc: { availableSeat: -1, numberOfStudents: 1 } }
+                        );
+
+                        // console.log('Update Result:', updateResult);
+                    }
+                }
             }
         })
 
 
 
+        // payment history  routes
+
+        app.get('/paymentHistory', verifyJwt, async (req, res) => {
+            const email = req.decoded.email;
+            if (email) {
+                const result = await enrolledClassesCollection.find({ email }).sort({ _id: -1 }).toArray()
+                res.send(result)
+            }
+        })
+
+
+        //enrolled classes
+        app.get('/enrolledClasses', async (req, res) => {
+            // const email = req.decoded.email
+            try {
+                const pipeline = [
+                    {
+                        $addFields: {
+                            BookedId: {
+                                $map: {
+                                    input: '$BookedId',
+                                    as: 'itemId',
+                                    in: { $toObjectId: '$$itemId' }
+                                }
+                            }
+                        }
+                    },
+                    {
+                        $lookup: {
+                            from: 'Languages',
+                            localField: 'BookedId',
+                            foreignField: '_id',
+                            as: 'bookedData'
+                        }
+                    },
+                    {
+                        $unwind: '$bookedData'
+                    },
+                    {
+                        $group: {
+                            _id: '$bookedData.foreignLanguageName',
+                            teacherName: { $first: '$bookedData.teacherName' },
+                            languageImage: { $first: '$bookedData.languageImage' },
+                            numberOfStudents: { $first: '$bookedData.numberOfStudents' },
+                            availableSeat: { $first: '$bookedData.availableSeat' },
+                            email: { $first: '$bookedData.email' },
+                            price: { $first: '$bookedData.price' },
+                            status: { $first: '$bookedData.status' }
+                        }
+                    },
+                    {
+                        $project: {
+                            foreignLanguageName: '$_id',
+                            teacherName: '$teacherName',
+                            languageImage: '$languageImage',
+                            numberOfStudents: '$numberOfStudents',
+                            availableSeat: '$availableSeat',
+                            email: '$email',
+                            price: '$price',
+                            status: '$status',
+                            _id: 0
+                        }
+                    }
+                ];
+
+                const result = await enrolledClassesCollection.aggregate(pipeline).toArray();
+                res.send(result);
+            } catch (error) {
+                console.error('Error:', error);
+                res.status(500).send('An error occurred');
+            }
+        });
+
+        // Instructors routes
+        app.post('/AddCourse', verifyJwt, async (req, res) => {
+            const email = req.decoded.email
+            const course = req.body
+            if (email === course.email) {
+                const result = await LanguageCollection.insertOne(course)
+                res.send(result);
+            }
+        })
+
+        // All Classes That I posted
+        app.get('/MyClasses', verifyJwt, async (req, res) => {
+            const email = req.decoded.email
+
+            if (email) {
+                const result = await LanguageCollection.find({ email }).toArray()
+                res.send(result);
+            }
+        })
+
+        // Update my Classes Data
+        app.patch('/updateData/:id', verifyJwt, async (req, res) => {
+            const id = req.params.id;
+            const newData = req.body;
+            const updateDoc = {
+                $set: {
+                    languageImage: newData.languageImage,
+                    foreignLanguageName: newData.foreignLanguageName,
+                    teacherName: newData.teacherName,
+                    courseEnrollDeadline: newData.courseEnrollDeadline,
+                    courseDetails: newData.courseDetails,
+                    email: newData.email,
+                    availableSeat: newData.availableSeat,
+                    price: newData.price,
+
+                }
+            }
+            const query = { _id: new ObjectId(id) };
+            const result = await LanguageCollection.updateOne(query, updateDoc);
+
+            res.send(result);
+        });
         // await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
     } finally {
